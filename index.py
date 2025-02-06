389fb1cf5dc5d437cc49628e16570a9455b318e8
Saranya-Gunnam <168226887+Saranya-Gunnam@users.noreply.github.com>
2025-02-06 11:22:40 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,61 @@
+print(2)
+print(1,2,3,4,5,6)
+print(3+6)
+print(3-6)
+print(3/6)
+print(3*6)
+print(3**6)
+print(3//6)
+print(3%6)
+
+print(type(3))
+print(type(3.7))
+print(type(3.0))
+print(5/2)
+print(5//2)
+print(5/1)
+#print(5/0)
+
+num1=1234567
+num2=3456789
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1//num2)
+print(num1*num2)
+
+a=1 
+b=2
+a,b=3,4
+print(a,b)
+print("saranya")
+saranya="saranya_gunnam"
+print((saranya))
+print(type(saranya))
+print(len(saranya))
+
+str1 = 'Party ledha Pushpa'
+print(str1[0])
+print(str1[9])
+print(len(str1))
+
+print(str1[17])
+#print(str1[18])
+
+print(str1[3: 8])
+print(str1[3: ])
+print(str1[:10])
+print(str1[:])
+print(str1[3: 1000])
+
+#Negative indexes
+
+print(str1[-8: -2])
+print("Check",str1[-2: -8: -1])
+
+#Step
+print(str1[0: 15])
+print(str1[0: 15: 2])
+print(str1[0: 15: 3])
+print(str1[-2: -8: -1])
+
